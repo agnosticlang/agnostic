715736678a3fd8dff80301a7743563452a2a61dd
AnmiTaliDev <anmitali198@gmail.com>
2026-07-14 17:56:32 +0500

    reinit

--- src/llvm/mod.rs
+++
@@ -1,6 +0,0 @@
-// LLVM IR Backend
-// Author: AnmiTaliDev <anmitalidev@nuros.org>
-
-pub mod ir_generator;
-
-pub use ir_generator::LLVMIRGenerator;
--- src/nvm/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod codegen;
-pub mod asm_generator;
-
-pub use codegen::NVMCodeGen;
-pub use asm_generator::NVMAssemblyGenerator;
